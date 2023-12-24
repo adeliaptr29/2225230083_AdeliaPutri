ed182c7e125d181f94f0e5a6bbd48092ae8c0ead
adeliaptr29 <adelia.pt29@gmail.com>
2023-12-24 15:07:28 +0700

    commit on "main"

--- resources/views/warga/index.blade.php
+++ resources/views/warga/index.blade.php
@@ -136,10 +136,6 @@
     <marquee direction="right" behavior="scroll" scrolldelay="10">PASTIKAN DATAMU ADA PADA TABEL DI BAWAH INI! AYOO SEGERA
         TAMBAHKAN DATAMU!!!</marquee>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
-
     <div class="tombol-logout">
         <form action="{{ route('logout') }}" method="POST">
             @csrf
@@ -191,7 +187,7 @@
                         <tr>
                             <td>{{ $loop->iteration }}</td>
                             <td>{{ $w->Nama }}</td>
-                            <td><img src="{{ asset('images/' . $w->Gambar) }}"></td>
+                            <td><img src="{{ asset($w->Gambar) }}" alt="{{ $w->Nama }}"></td>
                             <td>{{ $w->Email }}</td>
                             <td>{{ $w->Instagram }}</td>
                             <td>{{ $w->Tiktok }}</td>
